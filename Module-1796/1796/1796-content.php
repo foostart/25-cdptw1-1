d7b9bdd60b5ec2dd223d4cf9cc66f6c06d3aa368
Nguyễn Thanh Phong <ntp4994@gmail.com>
2019-09-23 21:37:18 +0700

    commit

---
+++ Module-1796/1796/1796-content.php
@@ -0,0 +1,56 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-1796">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Sign In</h3>
+                <div class="a">
+                    <span style="width: 100%;">You can use these credentials for demo testing:</span>
+                </div>
+                <div class="b">
+                    <span style="width: 40%;">
+                        <b>Dealer:</b><br>
+                        dealer<br>
+                        dealer
+                    </span>
+
+                    <span style="width: 40%;">
+                        <b>User:</b><br>
+                        demo<br>
+                        demo
+                    </span>
+                </div>
+            </div>
+            <div class="stm-login-form">
+                <form method="post">
+                    <div class="form-group">
+                        <h4>Login or E-mail</h4>
+                        <input type="text" name="stm_user_login" placeholder="Enter login or E-mail">
+                    </div>
+                    <div class="form-group">
+                        <h4>Password</h4>
+                        <input type="password" name="stm_user_password" placeholder="Enter password">
+                    </div>
+                    <div class="form-group form-checker">
+                        <label>
+                            <span class="checker"><input type="checkbox" name="stm_remember_me">Remember me</span>
+                        </label>
+                        <div class="stm-forgot-password">
+                            <a href="#">Forgot Password</a>
+                        </div>
+                    </div>
+                    <input type="submit" value="Login">
+                    <span class="stm-listing-loader"><i class="stm-icon-load1"></i></span>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
