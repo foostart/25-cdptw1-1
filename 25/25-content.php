2af550f3b706743a1e1eb8bbafb11e2da4da0865
Nguyễn Thanh Phong <ntp4994@gmail.com>
2019-10-31 11:42:08 +0700

    commit

--- 25/25-content.php
+++ 25/25-content.php
@@ -178,16 +178,16 @@ $url_path = str_replace('\\', '/', $url_path);
                         <h3 class="title"><a href="#">Men's Blazer</a></h3>
                         <div class="stars">
                             <form>
-                                <input class="star star-1" id="star-1" type="radio" name="star" />
-                                <label class="star star-1" for="star-1"></label>
-                                <input class="star star-2" id="star-2" type="radio" name="star" />
-                                <label class="star star-2" for="star-2"></label>
-                                <input class="star star-3" id="star-3" type="radio" name="star" />
-                                <label class="star star-3" for="star-3"></label>
-                                <input class="star star-4" id="star-4" type="radio" name="star" />
-                                <label class="star star-4" for="star-4"></label>
-                                <input class="star star-5" id="star-5" type="radio" name="star" />
-                                <label class="star star-5" for="star-5"></label>
+                                <input class="star star-21" id="star-21" type="radio" name="star" />
+                                <label class="star star-21" for="star-21"></label>
+                                <input class="star star-22" id="star-22" type="radio" name="star" />
+                                <label class="star star-22" for="star-22"></label>
+                                <input class="star star-23" id="star-23" type="radio" name="star" />
+                                <label class="star star-23" for="star-23"></label>
+                                <input class="star star-24" id="star-24" type="radio" name="star" />
+                                <label class="star star-24" for="star-24"></label>
+                                <input class="star star-25" id="star-25" type="radio" name="star" />
+                                <label class="star star-25" for="star-25"></label>
                             </form>
                         </div>
                         <br>
